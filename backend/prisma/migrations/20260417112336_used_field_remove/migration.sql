5854e000247c3932e9b1c905902546bde514bf82
ashif-costa0770 <ashifcostatechnolab@gmail.com>
2026-04-21 12:35:03 +0530

    Clean migration reset

--- backend/prisma/migrations/20260417112336_used_field_remove/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `used` on the `passwordresettoken` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `passwordresettoken` DROP COLUMN `used`;
